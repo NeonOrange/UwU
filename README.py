0389c4e145ffb427bcd838d8b2dbda6e6bbe551b
NeonOrange <60914618+NeonOrange@users.noreply.github.com>
2020-02-11 02:12:34 -0500

    Add files via upload

---
+++ README.py
@@ -0,0 +1,4 @@
+###                   Hello, this is my first project!              ###
+### The program is not optimized for any inputs other than integers ###
+### Please give me some suggestions to cover the error cases thanks ###
+
